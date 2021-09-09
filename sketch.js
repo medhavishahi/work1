4d00847dc8c83f4327df101a2cdc71f692cc6a9e
medhavishahi <medhavishahi1485@gmail.com>
2021-09-09 18:47:36 +0530

    first commit

---
+++ sketch.js
@@ -0,0 +1,60 @@
+
+var player;
+var score=0;
+var wormGroup;
+
+function preload(){
+  swampImage=loadImage("images/background.jpg");
+  playerImage=loadImage("images/boy cartoon.jpg");
+  wormImage=loadImage("images/chocolate.jpg");
+}
+
+function setup() {
+createCanvas(600,600);
+swamp=createSprite(300,300);
+swamp.scale=2.6;
+swamp.addImage(swampImage);
+player = createSprite(40,550,30,30); 
+player.addImage(playerImage);
+player.scale=0.1;
+wormGroup= new Group();
+}
+
+function draw() {
+background("black");  
+stroke("red")
+noFill();
+ellipse(100,350,40,30);
+player.x= mouseX;
+player.y= mouseY;
+
+
+
+generateWorms();
+
+for(var i = 0 ; i< (wormGroup).length ;i++){
+  var temp = (wormGroup).get(i) ;
+  if (player.isTouching(temp)) {
+    score++;
+    temp.destroy();
+    temp=null;
+    }   
+  }
+
+  drawSprites();
+  textSize(20);
+  text("Worms destroyed: "+score,350,50);
+}
+
+function generateWorms(){
+if(frameCount % 30===0){
+  console.log(frameCount);
+  var worm = createSprite(random(40,380),random(290,380),40,5);
+  worm.addImage(wormImage);
+worm.scale=random(0.1,0.3)
+  worm.shapeColor="green"
+  worm.velocityX=random(-4,4);
+  worm.velocityY=random(-4,4);
+  wormGroup.add(worm);
+  }
+}
